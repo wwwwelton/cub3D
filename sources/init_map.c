dbfd08944b959f9e93afdd2d51330c8dda49e40f
VLN37 <54167882+VLN37@users.noreply.github.com>
2022-01-14 23:56:54 -0300

    feat: flawed map validator

--- sources/init_map.c
+++ sources/init_map.c
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:17:47 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/01/14 20:25:00 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/01/14 23:43:21 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,15 +113,103 @@ void	outline_polygon(char **map)
 					map[x][y] = EDGE;
 		y = -1;
 	}
-	int	i = -1;
-	printf("outline\n");
-	while (map[++i])
-		printf("%s\n", map[i]);
+}
+
+t_bool	ftex_is_in_set(char c, char *set)
+{
+	if (!set)
+		return (false);
+	while (*set)
+	{
+		if (c == *set)
+			return (true);
+		set++;
+	}
+	return (false);
+}
+
+int	check_end(char **map, int x, int y)
+{
+	return ((y > 0 && map[x][y + 1] == '!' && map[x][y - 1] == '!')
+	|| (x > 0 && map[x + 1] && map[x + 1][y] == '!' && map[x - 1][y] == '!')
+	|| (x > 0 && map[x - 1][y] == '!' && map[x][y + 1] == '!')
+	|| (x > 0 && y > 0 && map[x][y - 1] == '!' && map[x - 1][y] == '!')
+	|| (y > 0 && map[x + 1] && map[x + 1][y] == '!' && map[x][y - 1] == '!')
+	|| (map[x + 1] && map[x + 1][y] == '!' && map[x][y + 1] == '!'));
+}
+
+t_bool	crawl_polygon(char **map, int x, int y)
+{
+	while (1)
+	{
+		if (map[x + 1] && map[x + 1][y] == EDGE)
+			map[x++][y] = '!';
+		else if (y > 0 && map[x][y - 1] == EDGE)
+			map[x][y--] = '!';
+		else if (x > 0 && map[x - 1][y] == EDGE)
+			map[x--][y] = '!';
+		else if (map[x][y + 1] == EDGE)
+			map[x][y++] = '!';
+		else if (check_end(map, x, y))
+		{
+			printf("success\n");
+			return (true);
+		}
+		else
+		{
+			printf("failure\n");
+			return (false);
+		}
+	}
+}
+
+t_bool	is_player_polygon_closed(char **map)
+{
+	int	x;
+	int	y;
+
+	x = -1;
+	y = -1;
+	while (map[++x])
+	{
+		printf("%s\n", map[x]);
+		while (map[x][++y])
+			if (ftex_is_in_set(map[x][y], "NSEW"))
+				break ;
+	if (ftex_is_in_set(map[x][y], "NSEW"))
+				break ;
+		y = -1;
+	}
+	if (x == 0 || map[x][y + 1] == '\0')
+		return (false);
+	while (map[x][y] && map[x][y] != '9')
+		y++;
+	if (crawl_polygon(map, x, y))
+		return (true);
+	return (false);
 }
 
 void	map_validation(char **map)
 {
 	outline_polygon(map);
+	is_player_polygon_closed(map);
+	printf("outline\n");
+	int	i = -1;
+	int	j = -1;
+	while (map[++i])
+	{
+		while (map[i][++j])
+		{
+			if (map[i][j] == '!')
+				printf("\e[42m%c\e[0m", map[i][j]);
+			else if (map[i][j] == ' ')
+				printf("%c", ' ');
+			else
+				printf("\e[41m%c\e[0m", map[i][j]);
+		}
+		printf("\n");
+		j = -1;
+	}
 }
 
 void	init_map(t_data *data, char **argv)
