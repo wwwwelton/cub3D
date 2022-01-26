2c92f75eec53e3656f8d65ede0d2962772e3e177
wleite <wleite@student.42sp.org.br>
2022-01-26 17:08:25 -0300

    test: comment print map delay

--- sources/print_map.c
+++ sources/print_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:17:47 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/01/21 08:33:08 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/01/26 15:54:52 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	print_colored_map(char **map)
 	{
 		while (map[i][++j])
 		{
-			usleep(500);
+			// usleep(500);
 			if (map[i][j] == OUTER)
 				printf("\e[42m%c\e[0m", map[i][j]);
 			else if (map[i][j] == INNER)
