6b045a676e9e9e0fcc81f92d1a89a7d5bb9bb4de
VLN37 <54167882+VLN37@users.noreply.github.com>
2022-02-05 02:05:51 -0300

    refactor: draw minimap to layer

--- sources/draw/draw.c
+++ sources/draw/draw.c
@@ -17,9 +17,11 @@ static void	reinit_images(t_data *data)
 	mlx_destroy_image(data->mlx.mlx_ptr, data->img[PLAYER].img_ptr);
 	mlx_destroy_image(data->mlx.mlx_ptr, data->img[RAYS].img_ptr);
 	mlx_destroy_image(data->mlx.mlx_ptr, data->img[SCREEN].img_ptr);
+	mlx_destroy_image(data->mlx.mlx_ptr, data->img[MAP].img_ptr);
 	init_img(data, &data->img[PLAYER], IMG_WIDTH, IMG_HEIGHT);
 	init_img(data, &data->img[RAYS], IMG_WIDTH, IMG_HEIGHT);
 	init_img(data, &data->img[SCREEN], IMG_WIDTH, IMG_HEIGHT);
+	init_img(data, &data->img[MAP], IMG_WIDTH, IMG_HEIGHT);
 }
 
 void	draw_game(t_data *data)
@@ -31,12 +33,12 @@ void	draw_game(t_data *data)
 	draw_player(data);
 	draw_rays(data);
 	draw_wall(data);
+	draw_map(data, data->map, -1, -1);
 	copy_layer(&data->img[BG], &data->img[SCREEN], 0, 0);
 	copy_layer(&data->img[FRAME], &data->img[SCREEN], 0, 0);
-	copy_layer(&data->img[MAP], &data->img[SCREEN], 0, 0);
 	copy_layer(&data->img[PLAYER], &data->img[SCREEN], 0, 0);
 	copy_layer(&data->img[RAYS], &data->img[SCREEN], 0, 0);
-	draw_map(data, data->map, -1, -1);
+	copy_layer(&data->img[MAP], &data->img[SCREEN], 0, 0);
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, data->img->img_ptr, 0, 0);
 	reinit_images(data);
 }
--- sources/draw/draw_map.c
+++ sources/draw/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:07:01 by wleite            #+#    #+#             */
-/*   Updated: 2022/02/05 02:02:07 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/02/05 02:02:48 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void	init_draw_map(t_data *data, int *height, int *len, t_xy *pl)
 	*len = ft_strlen(data->map[0]);
 	*pl = get_coordinates(data->map, 0, 0);
 	vars = fillparams(502, 55, 10 * 23, 10 * 23);
-	fill(&data->img[SCREEN], vars, BLACKB);
+	fill(&data->img[MAP], vars, BLACKB);
 }
 
 void	draw_map(t_data *data, char **map, int x, int y)
@@ -48,13 +48,13 @@ void	draw_map(t_data *data, char **map, int x, int y)
 			vars = fillparams(WIN_HEIGHT - 270 + 11 * x, 50 + 11 * y, 10, 10);
 			if (pl.x + x - 10 < 0 || pl.y + y - 10 < 0
 				|| pl.x + x - 9 > height || pl.y + y - 9 > len)
-				fill(&data->img[SCREEN], vars, 0x5c5c5c);
+				fill(&data->img[MAP], vars, 0x5c5c5c);
 			else if (ftex_is_in_set(map[pl.x + x - 10][pl.y + y - 10], "@! "))
-				fill(&data->img[SCREEN], vars, 0x1b4382);
+				fill(&data->img[MAP], vars, 0x1b4382);
 			else if (ftex_is_in_set(map[pl.x + x - 10][pl.y + y - 10], "NSEW"))
-				fill(&data->img[SCREEN], vars, YELLOW);
+				fill(&data->img[MAP], vars, YELLOW);
 			else
-				fill(&data->img[SCREEN], vars, 0x5c5c5c);
+				fill(&data->img[MAP], vars, 0x5c5c5c);
 		}
 		y = -1;
 	}
