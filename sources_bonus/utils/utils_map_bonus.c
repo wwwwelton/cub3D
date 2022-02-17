b3a30d786f13100abd4a78c1bb57554b88dad29c
wleite <wleite@student.42sp.org.br>
2022-02-17 03:13:33 -0300

    refactor: optimized map_height and map_width

--- sources_bonus/utils/utils_map_bonus.c
+++ sources_bonus/utils/utils_map_bonus.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:07:01 by wleite            #+#    #+#             */
-/*   Updated: 2022/02/16 22:59:51 by wleite           ###   ########.fr       */
+/*   Updated: 2022/02/17 03:12:01 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,24 @@
 
 int	map_height(t_data *data)
 {
-	int	i;
+	static int	map_height = -1;
 
-	i = 0;
-	while (data->map[i])
-		i++;
-	return (i);
+	if (map_height == -1)
+	{
+		map_height = 0;
+		while (data->map[map_height])
+			map_height++;
+	}
+	return (map_height);
 }
 
 int	map_width(t_data *data)
 {
-	return (ft_strlen(data->map[0]));
+	static int	map_width = -1;
+
+	if (map_width == -1)
+		map_width = ft_strlen(data->map[0]);
+	return (map_width);
 }
 
 int	is_inside_map(float x, float y, t_data *data)
