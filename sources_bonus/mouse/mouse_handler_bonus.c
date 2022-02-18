9876f86b62bf2ac0b541e6059f0d06411f073861
wleite <wleite@student.42sp.org.br>
2022-02-17 22:17:58 -0300

    refactor: changed mouse algorithm

--- sources_bonus/mouse/mouse_handler_bonus.c
+++ sources_bonus/mouse/mouse_handler_bonus.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 09:22:26 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/02/15 00:25:38 by wleite           ###   ########.fr       */
+/*   Updated: 2022/02/17 22:15:01 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,19 @@ void	check_mouse_visibility(t_data *data)
 static void	mouse_handler_x(int x, int mid_x, int mid_y, t_data *data)
 {
 	static int	new_x;
+	int			delta;
+	float		increment;
 	t_player	*player;
 
 	player = &data->player;
-	if (x != new_x && (abs(new_x - x) > 30))
+	delta = abs(new_x - x);
+	if (x != new_x && delta)
 	{
+		increment = (data->frame.delta_time * player->turn_speed / 250) * delta;
 		if (new_x > x)
-			player->rot_angle -= player->turn_speed / 5;
+			player->rot_angle -= increment;
 		if (new_x < x)
-			player->rot_angle += player->turn_speed / 5;
+			player->rot_angle += increment;
 		if (x > mid_x || x < mid_x)
 			mlx_mouse_move(data->mlx.mlx_ptr, data->mlx.win, mid_x, mid_y);
 		new_x = mid_x;
@@ -41,15 +45,9 @@ static void	mouse_handler_x(int x, int mid_x, int mid_y, t_data *data)
 static void	mouse_handler_y(int y, int mid_x, int mid_y, t_data *data)
 {
 	static int	new_y;
-	t_player	*player;
 
-	player = &data->player;
-	if (y != new_y && (abs(new_y - y) > 30))
+	if (y != new_y && abs(new_y - y))
 	{
-		if (new_y > y)
-			player->view_angle += player->look_speed / 5;
-		if (new_y < y)
-			player->view_angle -= player->look_speed / 5;
 		if (y > mid_y || y < mid_y)
 			mlx_mouse_move(data->mlx.mlx_ptr, data->mlx.win, mid_x, mid_y);
 		new_y = mid_y;
