e6cce29ecbb5af145e9bc7c51148d06a503e69b8
VLN37 <jofelipe@student.42sp.org.br>
2022-02-18 22:47:30 -0300

    fix: sprite distance order

--- sources_bonus/sprite/sprite_utils_bonus.c
+++ sources_bonus/sprite/sprite_utils_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sprite_utils_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:07:01 by wleite            #+#    #+#             */
-/*   Updated: 2022/02/18 19:56:23 by wleite           ###   ########.fr       */
+/*   Updated: 2022/02/18 22:29:53 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,12 @@ void	sort_visible_sprites(t_sprite *visible_sprites, int num_sprites)
 	t_sprite	temp;
 
 	i = -1;
-	while (++i < num_sprites - 1)
+	while (++i < num_sprites)
 	{
 		j = -1;
 		while (++j < num_sprites)
 		{
-			if (visible_sprites[i].distance < visible_sprites[j].distance)
+			if (visible_sprites[i].distance > visible_sprites[j].distance)
 			{
 				temp = visible_sprites[i];
 				visible_sprites[i] = visible_sprites[j];
