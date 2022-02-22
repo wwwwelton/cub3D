357d40fa7758609f66191a6c1fa8d17f6369a8c9
wleite <wleite@student.42sp.org.br>
2022-02-22 04:13:46 -0300

    feat: added mutant animation

--- sources_bonus/utils/utils_animation_bonus.c
+++ sources_bonus/utils/utils_animation_bonus.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:07:01 by wleite            #+#    #+#             */
-/*   Updated: 2022/02/22 02:55:02 by wleite           ###   ########.fr       */
+/*   Updated: 2022/02/22 04:13:34 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,28 @@ static void	animate_guard(t_sprite *sprite, t_data *data)
 	if (data->frame.cycle % 10 == 0)
 		sprite->frame++;
 	sprite->frame = sprite->frame % 3;
+}
+
+static void	animate_mutant(t_sprite *sprite, t_data *data)
+{
+	if (sprite->in_range && !sprite->hitted)
+		sprite->texture = SPT_MUT2;
+	if (sprite->hitted)
+	{
+		if (sprite->frame > 2)
+		{
+			sprite->collidable = 0;
+			return ;
+		}
+		if (sprite->frame == 0)
+			sprite->texture = SPT_MUT3;
+		if (sprite->frame == 1)
+			sprite->texture = SPT_MUT4;
+		if (sprite->frame == 2)
+			sprite->texture = SPT_MUT5;
+		if (data->frame.cycle % 6 == 0)
+			sprite->frame++;
+	}
 }
 
 static void	animate_weapon(t_data *data)
@@ -64,6 +86,8 @@ void	update_animations(t_data *data)
 	{
 		if (data->sprites[i].id == GUARD)
 			animate_guard(&data->sprites[i], data);
+		if (data->sprites[i].id == MUTANT)
+			animate_mutant(&data->sprites[i], data);
 	}
 	animate_weapon(data);
 }
