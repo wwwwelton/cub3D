2c508ad31e1777f68f6e469c05c1f3b4485903c4
VLN37 <jofelipe@student.42sp.org.br>
2022-02-24 15:52:54 -0300

    feat: dynamic background resolution

--- sources_bonus/draw/draw_menu_bonus.c
+++ sources_bonus/draw/draw_menu_bonus.c
@@ -3,14 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   draw_menu_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 00:18:15 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/02/15 00:25:38 by wleite           ###   ########.fr       */
+/*   Updated: 2022/02/24 15:40:59 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
+
+static void	put_resized_bg(t_data *data)
+{
+	float		scale_vert;
+	float		scale_horiz;
+	t_img		*newbg;
+	t_menu_elem	elem;
+
+	scale_vert = (float)WIN_HEIGHT / (float)data->img[MEN_BG].height;
+	scale_horiz = (float)WIN_WIDTH / (float)data->img[MEN_BG].width;
+	if (scale_horiz > scale_vert)
+		scale_vert = scale_horiz;
+	newbg = resize_image(&data->img[MEN_BG], scale_vert, data);
+	elem.src = newbg;
+	elem.dst = &data->img[SCREEN];
+	elem.x = middle_x(*newbg);
+	elem.y = middle_y(*newbg);
+	if (elem.y < 0)
+		elem.y = 0;
+	copy_layer(elem.src, elem.dst, elem.x, elem.y);
+	destroy_resized_image(newbg, data);
+}
 
 void	draw_menu(t_data *data)
 {
@@ -19,7 +41,11 @@ void	draw_menu(t_data *data)
 
 	img = data->img;
 	mlx = &data->mlx;
-	put_menu_item(data, MEN_BG, 0, 0);
+	if (data->img[MEN_BG].height != WIN_HEIGHT
+		|| data->img[MEN_BG].width != WIN_WIDTH)
+		put_resized_bg(data);
+	else
+		put_menu_item(data, MEN_BG, 0, 0);
 	if (data->game_running)
 		put_menu_item(data, RESUME, 0, -XOFF);
 	else
@@ -41,7 +67,10 @@ void	draw_options(t_data *data)
 	mlx = &data->mlx;
 	img = data->img;
 	(void) mlx;
-	put_menu_item(data, MEN_BG, 0, 0);
+	if (data->img[MEN_BG].height != WIN_HEIGHT)
+		put_resized_bg(data);
+	else
+		put_menu_item(data, MEN_BG, 0, 0);
 	put_menu_item(data, SENSI, OPTYOFF, XOFF * -3);
 	put_menu_item(data, MSPD, OPTYOFF, XOFF * -2);
 	put_menu_item(data, RSPD, OPTYOFF, XOFF * -1);
