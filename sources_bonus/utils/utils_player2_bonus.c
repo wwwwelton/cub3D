e5dfde7467cb95d23a82b2f3907cfc35598fd55c
VLN37 <jofelipe@student.42sp.org.br>
2022-02-26 19:21:01 -0300

    fix: map updater

--- sources_bonus/utils/utils_player2_bonus.c
+++ sources_bonus/utils/utils_player2_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_player2_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:07:01 by wleite            #+#    #+#             */
-/*   Updated: 2022/02/22 05:58:54 by wleite           ###   ########.fr       */
+/*   Updated: 2022/02/25 16:58:01 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void	update_player_matrix(t_data *data)
 	player_x = data->player.x / TILE_SIZE;
 	player_y = data->player.y / TILE_SIZE;
 	if ((xy.x != player_x || xy.y != player_y)
-		&& player_x < map_height(data) - 1
+		&& player_y < map_height(data) - 1
 		&& player_x >= 0 && player_y >= 0
-		&& player_y < map_width(data))
+		&& player_x < map_width(data))
 	{
 		if (data->map[player_y][player_x] == '0')
 		{
